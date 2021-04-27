ef2f1bc3feadb27b4b008408bd46940657fe1aa9
karim1706 <47336595+karim1706@users.noreply.github.com>
2021-04-27 19:38:57 +0200

    Delete Firebase.js

--- src/Firebase.js
+++
@@ -1,8 +0,0 @@
-const firebaseConfig = {
-    apiKey: "AIzaSyBemPoxGSjsWPeu8ZQyDinPxy9Y60QGp38",
-    authDomain: "clone-75089.firebaseapp.com",
-    projectId: "clone-75089",
-    storageBucket: "clone-75089.appspot.com",
-    messagingSenderId: "204779455119",
-    appId: "1:204779455119:web:3b2a56095d90125b7a66fa"
-  };
